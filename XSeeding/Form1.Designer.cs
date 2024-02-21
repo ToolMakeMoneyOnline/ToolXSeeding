448cb43bb5d2b0957198f0375380fd3581120e96
LATxBONA <leanhtunpqn2003@gmail.com>
2024-02-21 17:17:01 +0700

    test update 17:16 21/2/24

--- XSeeding/Form1.Designer.cs
+++ XSeeding/Form1.Designer.cs
@@ -28,34 +28,141 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.Username = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.displayname = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.cookie = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.Proxy = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.ChatGPTKey = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.note = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.Status = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
             this.SuspendLayout();
             // 
-            // button1
+            // button2
             // 
-            this.button1.Location = new System.Drawing.Point(351, 270);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(108, 61);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "button1";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Location = new System.Drawing.Point(31, 46);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(240, 43);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "CÀI ĐẶT ỨNG DỤNG";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button3.Location = new System.Drawing.Point(301, 46);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(173, 43);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "TRENDING";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(26, 114);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(227, 29);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Danh sách tài khoản";
+            // 
+            // listView1
+            // 
+            this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader1,
+            this.Username,
+            this.displayname,
+            this.cookie,
+            this.Proxy,
+            this.ChatGPTKey,
+            this.note,
+            this.Status});
+            this.listView1.FullRowSelect = true;
+            this.listView1.GridLines = true;
+            this.listView1.HideSelection = false;
+            this.listView1.Location = new System.Drawing.Point(31, 166);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(980, 366);
+            this.listView1.TabIndex = 4;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.Details;
+            // 
+            // columnHeader1
+            // 
+            this.columnHeader1.Text = "Profiled";
+            this.columnHeader1.Width = 100;
+            // 
+            // Username
+            // 
+            this.Username.Text = "Username";
+            this.Username.Width = 100;
+            // 
+            // displayname
+            // 
+            this.displayname.Text = "Tên hiển thị";
+            this.displayname.Width = 120;
+            // 
+            // cookie
+            // 
+            this.cookie.Text = "cookie";
+            this.cookie.Width = 100;
+            // 
+            // Proxy
+            // 
+            this.Proxy.Text = "Proxy";
+            this.Proxy.Width = 100;
+            // 
+            // ChatGPTKey
+            // 
+            this.ChatGPTKey.Text = "ChatGPTKey";
+            this.ChatGPTKey.Width = 100;
+            // 
+            // note
+            // 
+            this.note.Text = "Ghi chú";
+            this.note.Width = 130;
+            // 
+            // Status
+            // 
+            this.Status.Text = "Trạng thái";
+            this.Status.Width = 100;
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.button1);
+            this.ClientSize = new System.Drawing.Size(1023, 544);
+            this.Controls.Add(this.listView1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.Name = "Form1";
             this.Text = "XSeeding";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
-
-        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.ColumnHeader columnHeader1;
+        private System.Windows.Forms.ColumnHeader Username;
+        private System.Windows.Forms.ColumnHeader displayname;
+        private System.Windows.Forms.ColumnHeader cookie;
+        private System.Windows.Forms.ColumnHeader Proxy;
+        private System.Windows.Forms.ColumnHeader ChatGPTKey;
+        private System.Windows.Forms.ColumnHeader note;
+        private System.Windows.Forms.ColumnHeader Status;
     }
 }
 
